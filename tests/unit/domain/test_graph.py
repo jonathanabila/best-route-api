46585a3892c8634d26fe1f62714cedaf8e2ec0a2
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-05-25 22:25:41 -0300

    test: ensure Graph persist_edge calls File with correct formatting

--- tests/unit/domain/test_graph.py
+++ tests/unit/domain/test_graph.py
@@ -12,5 +12,16 @@ class TestGraph(TestCase):
         mock_file.return_value = MagicMock()
 
         mock_file.return_value.get_file_content.return_value = "file_content"
-        self.assertIsNone(Graph())
+        self.assertIsInstance(Graph(), Graph)
         mock_dijsktra.assert_called_with("file_content")
+
+        self.graph = Graph()
+
+    @patch("src.domain.graph.FILE_PATH", "file_path")
+    @patch("src.domain.graph.File")
+    def test_persist_edge(self, mock_file):
+        mock_file.return_value = MagicMock()
+        self.assertIsNone(self.graph._persist_edge("source", "destination", 1))
+        mock_file.return_value.add_line.assert_called_with(
+            "source,destination,1", "file_path"
+        )
