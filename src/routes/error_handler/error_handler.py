e23cf69f153cb57e23dd9662a8bed106d34619bd
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-05-26 00:59:10 -0300

    refactor: adapts the error handler to the exception and code structure

--- src/routes/error_handler/error_handler.py
+++ src/routes/error_handler/error_handler.py
@@ -9,14 +9,11 @@ LOG = new(__name__)
 def setup_error_handler(app):
     def _make_error(_error):
         message = _error.message if hasattr(_error, "message") else "Invalid request"
-        code = None
-
-        if ":" in message:
-            code = message.split(":")[0]
-            message = message.split(":")[1]
+        code = _error.code if hasattr(_error, "code") else 500
 
         return {"errors": message, "code": code}
 
     @app.errorhandler(InternalException)
     def handle_error(err):
-        return jsonify(_make_error(err)), err.code
+        error = _make_error(err)
+        return jsonify(error), error["code"]
