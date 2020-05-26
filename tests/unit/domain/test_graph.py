6094945ab533ef65e7f447d8a9760bac5c4e5bf4
Jonathan Abila <31670446+jonathanabila@users.noreply.github.com>
2020-05-25 22:14:07 -0300

    test: ensure Graph calls Dijsktra with the output from File

--- tests/unit/domain/test_graph.py
+++ tests/unit/domain/test_graph.py
@@ -1,5 +1,16 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from src.domain.graph import Graph
 
 
 class TestGraph(TestCase):
-    pass
+    @patch("src.domain.graph.File")
+    @patch("src.domain.graph.Dijsktra.__init__")
+    def setUp(self, mock_dijsktra, mock_file) -> None:
+        mock_dijsktra.return_value = MagicMock()
+        mock_file.return_value = MagicMock()
+
+        mock_file.return_value.get_file_content.return_value = "file_content"
+        self.assertIsNone(Graph())
+        mock_dijsktra.assert_called_with("file_content")
